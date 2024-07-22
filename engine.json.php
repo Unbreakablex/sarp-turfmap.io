c3496671a9d4e40299e7752dd9fd793c6b56027e
Jaidel Rivas <100535112+Unbreakablex@users.noreply.github.com>
2024-07-22 18:19:29 -0400

    Update engine.json.php

--- engine.json.php
+++ engine.json.php
@@ -6,46 +6,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Tu lógica para generar la respuesta JSON
     $response = [
         "status" => "success",
-        "data" => {
-  "factions": [
+        "data" => "factions": [
+        {
+  "id": 7416,
+  "name": "Playboy 13",
+  "color": "000000",
+  "thread": "DISCORD.GG",
+  "turfIDs": [
+    "7416"
+  ],
+  "turfs": [
+    "5203 4391 5339 4387 5338 4548 5270 4556 5216 4549 5197 4535"
+  ],
+  "turfcenter": [
+    [
+      5260.5,
+      4494.333333333333
+    ]
+  ],
+  "turfPolygon": [],
+  "dimensions": [
+    [
+      142,
+      169
+    ]
+  ],
+  "limits": [
     {
-      "id": "0",
-      "name": "3rd Gate Piru",
-      "color": "000000",
-      "thread": "https://forum.ls-rp.com/viewtopic.php?f=5&t=644835",
-      "turfIDs": ["1316"],
-      "turfs": ["4872 4952 4956 4952 4955 4851 4824 4819 4809 4837 4784 4835 4717 4816 4698 4818 4698 4842 4688 4866 4679 4875 4678 4945 4694 4950 4783 4952 "],
-      "turfcenter": [[4773.9285714286,4879.2857142857]],
-      "turfPolygon":[],
-      "dimensions":[[278,136]],
-      "limits": [{"minx":"4678","miny":"4816","maxx":"4956","maxy":"4952"}]
-    },
-    {
-      "id": "2",
-      "name": "Los Zetas; Fuentes y Laresca Organización",
-      "color": "6B47B7",
-      "thread": "https://forum.ls-rp.com/viewtopic.php?f=5&t=593509",
-      "turfIDs": ["1337","1350"],
-      "turfs": [
-        "4082 4131 4089 3959 4154 3957 4255 3940 4254 4136 ",
-        "4979 4033 4949 4021 4958 3959 4990 3929 5041 3919 5118 3924 5283 4008 5262 4054 5213 4091 5152 4102 5102 4076 5069 4050 5023 4038 "
-      ],
-      "turfcenter": [
-        [4166.8,4024.6],
-        [5087.6153846154,4015.6923076923]
-      ],
-      "turfPolygon": [],
-      "dimensions": [
-        [173,196],
-        [334,183]
-      ],
-      "limits": [
-        {"minx":"4082","miny":"3940","maxx":"4255","maxy":"4136"},
-        {"minx":"4949","miny":"3919","maxx":"5283","maxy":"4102"}
-      ]
-    },
+      "minx": 5197,
+      "miny": 4387,
+      "maxx": 5339,
+      "maxy": 4556
+    }
   ]
 }
+  ]
     ];
 
     echo json_encode($response);
