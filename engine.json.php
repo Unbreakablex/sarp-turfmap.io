2f55ed9594afed2d801013c0cbb425c305352ac8
Jaidel Rivas <100535112+Unbreakablex@users.noreply.github.com>
2024-07-22 19:19:10 -0400

    Update engine.json.php

--- engine.json.php
+++ engine.json.php
@@ -1,13 +1,4 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Configura la cabecera para devolver JSON
-    header('Content-Type: application/json');
-
-    // Tu lógica para generar la respuesta JSON
-    $response = [
-        "status" => "success",
-        "data" => [
-            "factions": [
+"factions": [
         {
   "id": 0,
   "name": "playboys 13",
@@ -17,35 +8,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     "3782"
   ],
   "turfs": [
-    
+    "5216 4388 5337 4391 5340 4552 5271 4556 5258 4553 5238 4550 5217 4543"
   ],
   "turfcenter": [
     [
-      5
+      5268.142857142857,
+      4504.714285714285
     ]
   ],
   "turfPolygon": [],
   "dimensions": [
     [
-      
+      124,
+      168
     ]
   ],
   "limits": [
     {
-      
+      "minx": 5216,
+      "miny": 4388,
+      "maxx": 5340,
+      "maxy": 4556
     }
   ]
 }
   ]
-]
-        ]
-    ];
-
-    // Codifica y devuelve la respuesta JSON
-    echo json_encode($response, JSON_PRETTY_PRINT);
-} else {
-    // Devuelve un error 405 si se utiliza otro método HTTP
-    http_response_code(405);
-    echo json_encode(["error" => "Method not allowed"]);
-}
-?>
